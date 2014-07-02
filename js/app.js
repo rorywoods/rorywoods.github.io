9c4191d28d3549bfde6bdffe20b6408c3a71c729
rory woods <rory.woods@gmail.com>
2014-07-02 15:04:37 -0400

    refresh

---
+++ js/app.js
@@ -0,0 +1,95 @@
+(function ($) {
+  "use strict;"
+  
+  $(document).ready(function() {
+
+    // prevent the # links to scroll to the top of the page
+    $("[href=#]").click(function(e) {
+      e.preventDefault();
+    });
+
+    $("[data-toggle=popover]").popover();
+    
+    $("[data-toggle=tooltip]").tooltip();
+
+    $('.vertical-center').flexVerticalCenter('padding-top');
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('header').addClass('stuck');
+      } else {
+        $('header').removeClass('stuck');
+      }
+    });
+
+    $('.smoothScroll').smoothScroll({
+      speed: 600,
+      offset: -80
+    });
+
+    $('.venobox').venobox();
+
+    /* Form submission code */
+    // Get the form.
+    var form = $('#theme-contact');
+
+    // Get the messages div.
+    var formMessages = $('#theme-form-messages');
+
+    // Set up an event listener for the contact form.
+    $(form).submit(function(e) {
+      // Stop the browser from submitting the form.
+      e.preventDefault();
+
+      // Serialize the form data.
+      var formData = $(form).serialize();
+
+      // Submit the form using AJAX.
+      $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+      })
+      .done(function(response) {
+        // Set the message text.
+        $(formMessages).html(response);
+
+        // Clear the form.
+        $('#contact-name').val('');
+        $('#contact-email').val('');
+        $('#contact-subject').val('');
+        $('#contact-message').val('');
+      })
+      .fail(function(data) {
+        // Set the message text.
+        if (data.responseText !== '') {
+          $(formMessages).html(data.responseText);
+        } else {
+          $(formMessages).html('<div class="alert alert-danger margin-top-40"><button type="button" class="close" data-dismiss="alert">×</button><strong>Error!</strong><br /> Error!</div>');
+        }
+      });
+
+    });
+
+  });
+  
+})(jQuery);
+
+jQuery(window).load(function() {
+  "use strict";
+
+  $('.fade-in').fadeIn(1500);
+
+  // Parallax
+  if ($(window).width() >= 991 && !navigator.userAgent.match(/(Android|iPod|iPhone|iPad|IEMobile|Opera Mini)/)) {
+    $(window).stellar({
+      horizontalScrolling: false,
+      horizontalOffset: 0
+    });
+  }
+
+  $(window).resize(function() {
+    ($(window).width() < 991 || navigator.userAgent.match(/(Android|iPod|iPhone|iPad|IEMobile|Opera Mini)/)) ? $(window).stellar('destroy') : $(window).stellar({ horizontalScrolling: false, horizontalOffset: 0 });
+  });
+
+});
---
+++ mail.php
@@ -0,0 +1,58 @@
+<?php  
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if ($_POST['contact-name'] != "") {
+      $_POST['name'] = filter_var($_POST['contact-name'], FILTER_SANITIZE_STRING);
+      if ($_POST['contact-name'] == "") {
+        $errors .= 'Please enter a valid name.<br/><br/>';
+      }
+    } else {
+      $errors .= 'Please enter your name.<br/>';
+    }
+
+    if ($_POST['contact-email'] != "") {
+      $email = filter_var($_POST['contact-email'], FILTER_SANITIZE_EMAIL);
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors .= "$email is <strong>NOT</strong> a valid email address.<br/><br/>";
+      }
+    } else {
+      $errors .= 'Please enter your email address.<br/>';
+    } 
+
+    if ($_POST['contact-subject'] != "") {
+      $_POST['contact-subject'] = filter_var($_POST['contact-subject'], FILTER_SANITIZE_STRING);
+      if ($_POST['contact-subject'] == "") {
+        $errors .= 'Please enter a subject.<br/>';
+      }
+    } else {
+      $errors .= 'Please enter a subject.<br/>';
+    }
+
+    if ($_POST['contact-message'] != "") {
+      $_POST['contact-message'] = filter_var($_POST['contact-message'], FILTER_SANITIZE_STRING);
+      if ($_POST['contact-message'] == "") {
+        $errors .= 'Please enter a message to send.<br/>';
+      }
+    } else {
+      $errors .= 'Please enter a message to send.<br/>';
+    }
+
+    if (!$errors) {
+      $mail_to = 'youremail@address.com';
+      $subject = "Ernesto HTML: " . $_POST['contact-subject'];
+      $message  = 'From: ' . $_POST['contact-name'] . "\n";
+      $message .= 'Email: ' . $_POST['contact-email'] . "\n\n";
+      $message .= "Message:\n" . $_POST['contact-message'] . "\n";
+      $header = "From:" . $_POST['contact-email'] . "\r\n";
+      if (mail($mail_to, $subject, $message, $header)) {
+        echo '<div class="alert alert-success margin-top-40"><button type="button" class="close" data-dismiss="alert">×</button><strong>Success!</strong> Thank you for your email.</div>';
+      } else {
+        echo '<div class="alert alert-danger margin-top-40"><button type="button" class="close" data-dismiss="alert">×</button><strong>Error in submiting! Please fill out the form again.</strong><br /></div>';
+      }
+    } else {
+      echo '<div class="alert alert-danger margin-top-40"><button type="button" class="close" data-dismiss="alert">×</button><strong>Error!</strong><br /> ' . $errors . '</div>';
+    }
+  }
+
+?>
